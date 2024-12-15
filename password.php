d898a221c3a23e306dc4d59d61e8560bd81bfb78
SherMill <112295094+SherMill@users.noreply.github.com>
2024-12-15 11:35:01 -0500

    Update password.php

--- password.php
+++ password.php
@@ -1,13 +1,19 @@
 <?php
 
-$password = ['$2y$10$dOhVZs8KNZAcsqDK48p2Tuu1hXfMjeW0/RKc6XAczxVZ2zcoKP3Ta', 
-            '$2y$10$.mf15ul/lnyuHBym8en5d.uOaFksgl.xz/xT1ABUrH5Myvfi.kJJe', 
-            '$2y$10$98C2BSKINH9M5DXGtmMCHeIO1RCUofUr/BF7WGjJBPqGo01SK4NEO', 
-            '$2y$10$Ef8GNyXrNlNpcR0vnseWbOVbgmQrEzCvHTeC4kBHsdQwWdQ0QGuc.', 
-            '$2y$10$/ox0gWfr12HtD9PKZyxYgehSywFSGEZX0gCoZd5I9WoThliWvCy3S'];
+//Array containing the list of passwords
+$password = ['password123', 
+            'password124', 
+            'password125', 
+            'password126', 
+            'password127'];
 
+//Loops the array for each password
 foreach ($password as $password){
+    
+    //Hash the password using the default algorithm
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    //Outputs the original password and its hashed version
     echo "Password $password => Hash: $hashedPassword<br>";
 }
 
